b5204d11e4ad717d18ae8b3dbd2afa910754bea3
rigo <rigobertoramirez606@gmail.com>
2018-12-09 13:00:14 -0600

    made some notes and followed exaples in what primative and refrence types

---
+++ index.js
@@ -0,0 +1,39 @@
+//this is my first javascript code!
+console.log("Hello World");
+//Tips
+//*Use camel casing
+//*JS is case sensitive 
+
+//Primitive Types---
+//string
+let name = "Rigobeto";
+let lastName = "Ramirez-Pascual";
+//number
+let age = 23;
+const interestRate = 0.3;
+//boolean
+let isApproved = false;
+//object
+let selectColor = null;
+
+//refrence Types---
+//(1)--object
+let person = {
+    name: "Rigoberto",
+    age: 23
+};
+//Dot Notation (best practice)
+person.name = "Rigo";
+
+//Braket Notation
+let selection = "name";
+person["name"] = "Maria";
+console.log(person);
+
+//(2) Arrays (used to store a list)
+//*used to represent a list of items
+//*index is the location of the element starting with 0
+//*an array is a type of object
+let selectColors = ["red", "blue"];
+selectColors[2] = 1;
+console.log(selectColors.length);
